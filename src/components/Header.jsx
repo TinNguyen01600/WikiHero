859551fc776c43ab91caa555ab9d1e554dd4cf7f
TinNguyen <ductin16111@gmail.com>
2023-12-10 18:42:43 +0200

    add icon, logo and background image

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import hero from "../../src/img/hero.png"
+
+const Header = () => {
+    return (
+        <header className="center">
+            <img src={hero} alt="" />
+        </header>
+    )
+}
+export default Header
