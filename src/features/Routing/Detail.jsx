d311c075fd066cb4cc53975acf8d624106b29653
TinNguyen <ductin16111@gmail.com>
2023-12-14 02:19:40 +0200

    update Detail contain and its CSS

--- src/features/Routing/Detail.jsx
+++ src/features/Routing/Detail.jsx
@@ -1,33 +1,48 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 const Detail = ({ char }) => {
-    const alignment = char.biography.alignment === 'bad'
-        ? ' villain'
-        : char.biography.alignment === 'good'
-            ? ' hero'
-            : 'neutral'
+    const alignment =
+        char.biography.alignment === "bad"
+            ? " villain"
+            : char.biography.alignment === "good"
+                ? " hero"
+                : "neutral";
 
+    let aliases = ''
+    for (let i of char.biography.aliases) {aliases += i + '; '}
+    let height = ''
+    for (let i of char.appearance.height) {height += i + '; '}
+    let weight = ''
+    for (let i of char.appearance.weight) {weight += i + '; '}
     return (
         <div className="detail">
             <div className="detail-container">
                 <img src={char.images.lg} alt="" />
-                <Link to="/">Go back</Link>
+                <div className="Link">
+                    <Link to="/">
+                        <Button variant="contained" size="large" sx={{ bgcolor: '#6b6eb0' }}>
+                            Go back
+                        </Button>
+                    </Link>
+                </div>
             </div>
             <div className="detail-text">
                 <h1>{char.name}</h1>
                 <div>
-                    {char.name} is a character created by {char.biography.publisher}.
+                    {char.name} is a character created by{" "}
+                    {char.biography.publisher}.
                     <ul>
                         <li>Full Name: {char.biography.fullName}</li>
                         <li>First debuted: {char.biography.firstAppearance}</li>
-                        <li>Place of birth</li>
-                        <li>Race</li>
-                        <li>Height</li>
-                        <li>Weight</li>
+                        <li>Place of birth: {char.biography.placeOfBirth}</li>
+                        <li>Race: {char.appearance.race}</li>
+                        <li>Height: {height}</li>
+                        <li>Weight: {weight}</li>
                         <li>Alignment: {alignment}</li>
-                        <li>Work</li>
-                        <li>Residence</li>
-                        <li>Also known as: </li>
+                        <li>Work: {char.work.occupation}</li>
+                        <li>Residence: {char.work.base}</li>
+                        <li>Also known as: {aliases}</li>
                         <li>
                             Power Stats:
                             <ul>
